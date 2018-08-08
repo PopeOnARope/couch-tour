1c1b08cbfb4413d71e6be735970a77eaa2b0f69e
John Pope <john.pope@benefitfocus.com>
2018-08-08 14:15:13 -0400

    a bunch of stuff

--- server.js
+++ server.js
@@ -1,22 +1,42 @@
-const express = require("express");
-const fetch = require("node-fetch");
+const express = require('express');
+const fetch = require('node-fetch');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/api/hello", (req, res) => {
-  res.send({ express: "Hello From Express" });
+const bodyParser = require('body-parser');
+
+/** bodyParser.urlencoded(options)
+ * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+ * and exposes the resulting object (containing the keys and values) on req.body
+ */
+app.use(
+	bodyParser.urlencoded({
+		extended: true
+	})
+);
+
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
+
+app.get('/api/hello', (req, res) => {
+	res.send({ express: 'Hello From Express' });
 });
 
-app.get("/api/shows", (req, res) => {
-  debugger;
-  const _res = req.items.artists.map(artist =>
-    fetch(
-      `https://rest.bandsintown.com/artists/${name}/events?app_id=5edfb100a7e4e77bd4658d1184623cbf`
-    )
-  );
-  // console.log(_res);
-  res.send({ foo: "bar" });
+app.post('/api/shows', (req, res) => {
+	debugger;
+	const _res = req.body.items.map(artist =>
+		fetch(
+			`https://rest.bandsintown.com/artists/${artist.name
+				.replace(/\s+/g, '')
+				.replace(/\./g, '')
+				.toLowerCase()}/events?app_id=5edfb100a7e4e77bd4658d1184623cbf`
+		)
+	);
+	console.log(req.body);
+	res.send(_res);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
